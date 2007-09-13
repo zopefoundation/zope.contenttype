811ebe9ecd930ac5a6c83cad540bde201a0e6dbb
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-09-13 10:09:05 +0000

    Bump version for development

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name='zope.contenttype',
-    version='3.4.0',
+    version='3.4.1dev',
     url='http://pypi.python.org/pypi/zope.contenttype',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
