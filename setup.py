64241a875e2a9f426d62ee77822cd3320aa42c79
Tres Seaver <tseaver@palladion.com>
2010-04-06 03:04:38 +0000

    Comply with repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -41,7 +41,7 @@ setup(
     name='zope.contenttype',
     version='3.5.2dev',
     url='http://pypi.python.org/pypi/zope.contenttype',
-    author='Zope Corporation and Contributors',
+    author='Zope Foundation and Contributors',
     author_email='zope3-dev@zope.org',
     license='ZPL 2.1',
     classifiers=[
