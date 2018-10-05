94700ac79bf5f14a59f080da10783cbb92fe037d
Michael Howitz <mh@gocept.com>
2018-10-05 16:41:25 +0200

    Preparing release 4.4

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.contenttype',
-    version='4.3.1.dev0',
+    version='4.4',
     url='http://github.com/zopefoundation/zope.contenttype',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
